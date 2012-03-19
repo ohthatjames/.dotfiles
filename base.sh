9c1e54e718cec5b2050c80da9716a27253673571
James Hunt <ohthatjames@gmail.com>
2012-03-19 00:20:46 +0000

    Initial commit

---
+++ base.sh
@@ -0,0 +1,7 @@
+source ~/.dotfiles/path.sh
+source ~/.dotfiles/git.sh
+source ~/.dotfiles/homebrew.sh
+source ~/.dotfiles/ruby.sh
+source ~/.dotfiles/rvm.sh
+
+source ~/.dotfiles/ps1.sh
---
+++ git.sh
@@ -0,0 +1,7 @@
+# Aliases
+
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias gdci="git diff --cached -i"
+alias gdi="git diff -i"
+
