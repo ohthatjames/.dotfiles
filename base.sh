803858410e4c7bef5fbd599517e28148ad7ce32c
James Hunt <ohthatjames@googlemail.com>
2012-12-12 09:50:36 +0000

    Alphabetise files

--- base.sh
+++ base.sh
@@ -1,10 +1,9 @@
-source ~/.dotfiles/path.sh
 source ~/.dotfiles/delete_branch.sh
 source ~/.dotfiles/failing_features.sh
 source ~/.dotfiles/git.sh
 source ~/.dotfiles/homebrew.sh
+source ~/.dotfiles/path.sh
+source ~/.dotfiles/ps1.sh
 source ~/.dotfiles/rails.sh
 source ~/.dotfiles/ruby.sh
 source ~/.dotfiles/rvm.sh
-
-source ~/.dotfiles/ps1.sh
