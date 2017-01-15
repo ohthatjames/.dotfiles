ec998dd8c3631d8c1e28e250466d94bbc3890689
James Hunt <ohthatjames@gmail.com>
2017-01-15 02:19:20 +0000

    Add setup script
    Symlinks the atom snippets and installs some atom packages

---
+++ script/setup.sh
@@ -0,0 +1,12 @@
+
+# Symlink files
+ln -sf `pwd`/files/atom-snippets.cson ~/.atom/snippets.cson
+
+# Atom packages
+
+apm install language-rust
+apm install ohthatjames/letify
+apm install pigments
+apm install rails-rspec
+apm install rspec-snippets
+apm install ohthatjames/ruby-initializer-variables
