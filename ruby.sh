d083a884e63c85dbf676d80356728285f9625036
James Hunt <ohthatjames@gmail.com>
2017-01-21 21:22:27 +0000

    chruby

--- ruby.sh
+++ ruby.sh
@@ -4,3 +4,8 @@ alias rc="spring rails c"
 alias rg="spring rails g"
 alias rgm="spring rails g migration"
 alias rs="spring rspec"
+
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+
+chruby 2.3.1
