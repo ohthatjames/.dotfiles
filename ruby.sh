df710f2a1696ba5b7b03f2c594398ac51b9f8c38
James Hunt <ohthatjames@gmail.com>
2017-08-30 10:51:09 +0100

    Use bin/rails instead of spring rails in aliases

--- ruby.sh
+++ ruby.sh
@@ -1,9 +1,10 @@
 alias be="bundle exec"
-alias rdb="spring rake db:migrate"
-alias rc="spring rails c"
-alias rg="spring rails g"
-alias rgm="spring rails g migration"
-alias rs="spring rspec"
+alias rdb="bin/rails db:migrate"
+alias rdbt="bin/rails db:migrate RAILS_ENV=test"
+alias rc="bin/rails c"
+alias rg="bin/rails g"
+alias rgm="bin/rails g migration"
+alias rs="bin/rspec"
 
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
