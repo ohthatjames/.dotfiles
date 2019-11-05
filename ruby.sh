315296d6377908ec2be2669fa31f13b82cd5b193
James Hunt <ohthatjames@gmail.com>
2019-11-05 10:19:21 +0000

    Use spring instead of bin/rails
    For some reason this played merry hell with memory usage, so going back
    to the old way.

--- ruby.sh
+++ ruby.sh
@@ -1,10 +1,10 @@
 alias be="bundle exec"
-alias rdb="bin/rails db:migrate"
-alias rdbt="bin/rails db:test:prepare"
-alias rc="bin/rails c"
-alias rg="bin/rails g"
-alias rgm="bin/rails g migration"
-alias rs="bin/rspec"
+alias rdb="rake db:migrate"
+alias rdbt="rake db:migrate && rake db:test:prepare"
+alias rc="spring rails c"
+alias rg="spring rails g"
+alias rgm="spring rails g migration"
+alias rs="spring rspec"
 
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
