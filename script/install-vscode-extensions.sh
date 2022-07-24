5be415a4c07dfdc05ced2787117b308bc1380387
James Hunt <ohthatjames@gmail.com>
2022-07-24 14:38:53 +0100

    Initial VS Code settings and extensions

---
+++ script/install-vscode-extensions.sh
@@ -0,0 +1,4 @@
+code --install-extension allenshuber.quote-flipper
+code --install-extension ldrner.rspec-snippets-vscode
+code --install-extension noku.rails-run-spec-vscode
+code --install-extension tatosjb.rspec-quick-spec
