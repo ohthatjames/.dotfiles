4051f1650c7dd3641872db73f234513737bfdd89
James Hunt <ohthatjames@gmail.com>
2022-07-24 14:42:49 +0100

    Remove references to non-existent files

--- base.sh
+++ base.sh
@@ -3,7 +3,6 @@ source ~/.dotfiles/git_log.sh
 source ~/.dotfiles/homebrew.sh
 source ~/.dotfiles/path.sh
 source ~/.dotfiles/ruby.sh
-source ~/.dotfiles/run_features.sh
 
 if test -e `echo "~/.dotfiles/private/*.sh" | cut -d' ' -f1`
 then
--- ruby.sh
+++ ruby.sh
@@ -5,6 +5,3 @@ alias rc="spring rails c"
 alias rg="spring rails g"
 alias rgm="spring rails g migration"
 alias rs="spring rspec"
-
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
