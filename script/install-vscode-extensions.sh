5a4a9d9c74c9edb48709324de17a15e1099a69d6
James Hunt <ohthatjames@gmail.com>
2022-07-26 15:44:05 +0100

    More vscode extensions

--- script/install-vscode-extensions.sh
+++ script/install-vscode-extensions.sh
@@ -1,4 +1,6 @@
 code --install-extension allenshuber.quote-flipper
 code --install-extension ldrner.rspec-snippets-vscode
 code --install-extension noku.rails-run-spec-vscode
+code --install-extension stkb.rewrap
+code --install-extension sysoev.vscode-open-in-github
 code --install-extension tatosjb.rspec-quick-spec
