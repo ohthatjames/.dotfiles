5bf1ec6477522540a5da91339bbf9beb20b68637
James Hunt <ohthatjames@gmail.com>
2023-09-14 21:12:07 +0100

    Remove unused aliases

--- ruby.sh
+++ ruby.sh
@@ -1,7 +1,3 @@
 alias be="bundle exec"
 alias rdb="rake db:migrate"
 alias rdbt="rake db:migrate && rake db:test:prepare"
-alias rc="spring rails c"
-alias rg="spring rails g"
-alias rgm="spring rails g migration"
-alias rs="spring rspec"
